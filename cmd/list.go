5bf9b3bf62b5bfd2f0800345a8aefb9b3b520cbb
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-05-23 00:11:06 +0100

    feat: add total install size to list
    Signed-off-by: BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>

--- cmd/list.go
+++ cmd/list.go
@@ -21,20 +21,20 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Shows a list of installed versions of Proton.",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		// Read the install directory
 		installDir := backend.UsePath(viper.GetString("app.install_directory"), true)
-
-		// for every directory inside of the install directory,
-		// print the name of the directory.
 		dir, err := ioutil.ReadDir(installDir)
-
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
 		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{"Version", "Size", "Modified"})
+		table.SetHeader([]string{"Version", "Size", "Installed"})
 
+		// Get the sizes of all the installed Proton versions.
+		var totalSize int64
 		for _, d := range dir {
 
 			size, err := backend.DirSize(installDir + d.Name())
@@ -45,10 +45,14 @@ var listCmd = &cobra.Command{
 			}
 
 			hSize, hUnit := backend.HumanReadableSize(size)
+			totalSize += size
 
 			table.Append([]string{d.Name(), fmt.Sprintf("%v%s", hSize, hUnit), d.ModTime().Format("2006-01-02")})
 		}
 
+		tSize, tUnit := backend.HumanReadableSize(totalSize)
+
+		table.SetFooter([]string{"Total", fmt.Sprintf("%v%s", tSize, tUnit), " "})
 		table.Render()
 	},
 }
