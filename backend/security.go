de283449c0ed32f56c598593bf94c865756f6462
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-05-25 00:48:03 +0100

    refactor: adjust IsPreview and IsRoot
    Signed-off-by: BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>

--- backend/security.go
+++
@@ -1,9 +0,0 @@
-package backend
-
-import "os"
-
-// Detects if the binary is a preview version. (Identified by Version string)
-func IsPreview() bool {
-	Debug("IsPreview: Checking for preview version, detected: " + Version)
-	return Version == "0.0.1-next" && os.Getenv("PROTO_PREVIEW_CONSENT") != "true"
-}
--- main.go
+++ main.go
@@ -13,11 +13,14 @@ import (
 
 func main() {
 
-	backend.PreventRoot()
+	if backend.IsRoot() {
+		fmt.Println("Proto cannot not be run as root, please try again as a regular user without sudo.")
+		os.Exit(1)
+	}
 
 	if backend.IsPreview() {
 		fmt.Println("Detected a preview version of Proto! If you are a user, please download a stable version from GitHub directly.")
-		fmt.Println("If you are certain you want to run this preview build, set environment variable 'PROTO_PREVIEW_CONSENT' to 'true'")
+		fmt.Println("If you are certain you want to run this preview build, set environment variable 'PROTO_CONSENT' to 'true'")
 		os.Exit(1)
 	}
 
