88b0caf2479a6d61be7682773c3cdcc553dfe40c
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-05-25 01:16:17 +0100

    refactor: Fix system.go
    Signed-off-by: BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>

---
+++ backend/system.go
@@ -0,0 +1,37 @@
+package backend
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Restricts running the binary as a root user. (Identified by UID == 0)
+func IsRoot() bool {
+	return os.Geteuid() == 0
+
+}
+
+// Detects if the binary is a preview version. (Identified by Version string)
+func IsPreview() bool {
+	return Version == "0.0.1-next" && os.Getenv("PROTO_CONSENT") != "true"
+}
+
+// Prompt a user to confirm something
+func Prompt(message string, defaultValue bool) bool {
+	var response string
+
+	Debug("Prompt: Asking for user input")
+
+	fmt.Print(message)
+	fmt.Scanln(&response)
+
+	switch strings.ToLower(response) {
+	case "y", "yes":
+		return true
+	case "n", "no":
+		return false
+	default:
+		return defaultValue
+	}
+}
