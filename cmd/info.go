5ba9f8a7663f19fff989759f6e1a3ed3a3456bd3
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-05-30 00:54:06 +0100

    feat: add info cmd
    Signed-off-by: BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>

---
+++ cmd/info.go
@@ -0,0 +1,58 @@
+/*
+Copyright © 2022 BitsOfAByte
+
+*/
+package cmd
+
+import (
+	"ProtoSoftware/proto/shared"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// infoCmd represents the info command
+var infoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "Shows information about the given release.",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// If there are multiple sources, ask the user which one to use or use the flag.
+		var source int
+		sourceFlag, _ := cmd.Flags().GetInt("source")
+		if sourceFlag > 0 {
+			sourceLen := len(viper.GetStringSlice("app.sources"))
+
+			if sourceFlag >= sourceLen {
+				fmt.Println("There is no source at index", sourceFlag, "you only have", sourceLen, "sources.")
+				os.Exit(1)
+			}
+
+			source = sourceFlag - 1
+		} else {
+			source = shared.PromptSourceIndex()
+		}
+
+		// Fetch the release data.
+		data, err := shared.GetReleaseData(source, args[0])
+
+		if err != nil {
+			fmt.Println("That release does not exist on the given source.")
+		}
+
+		// Print the release data.
+		fmt.Println("Release:", data.GetTagName())
+		fmt.Println("Published:", data.GetPublishedAt().Format("2006-01-02 15:04:05"))
+		fmt.Println("Description:", data.GetBody())
+		fmt.Println("Install Command: proto install", data.GetTagName(), "-s", source+1)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(infoCmd)
+
+	infoCmd.Flags().IntP("source", "s", 0, "The index of the source to use.")
+}
